a7e3bfcfa0c90777e5700593a1bda2917d5707a8
Oybek <treyktoriya@gmail.com>
2023-05-13 13:29:54 +0500

    Main class added and project refactored

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import java.util.List;
+import java.util.Scanner;
+
+import static org.example.Inventory.*;
+
+public class Main {
+    public static void main(String[] args) {
+        menu();
+    }
+
+    private static void menu() {
+        System.out.println("Warehouse v1.0 (Created on May 11, 2023)");
+        System.out.println("Developer: Oybek Tulaboev (treyktoriya@gmail.com)");
+
+
+        System.out.println("\nAvailable commands:");
+        System.out.println("1. Search products");
+        System.out.println("2. List all products");
+        System.out.println("3. Exit");
+
+        List<Product> products = loadInventory();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("\nEnter command (1-3): ");
+            int command = scanner.nextInt();
+            switch (command) {
+                case 1:
+                    searchProducts(products);
+                    break;
+                case 2:
+                    listProducts(products);
+                    break;
+                case 3:
+                    System.out.println("Exiting application...");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Invalid command. Please enter a valid command (1-3).");
+                    break;
+            }
+        }
+    }
+}
